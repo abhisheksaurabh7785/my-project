09cf06acde5bbc4929d385219f6c0d874409f176
abhisheksaurabh7785 <72158831+abhisheksaurabh7785@users.noreply.github.com>
2020-10-16 20:28:15 +0530

    Update update.php

--- update.php
+++ update.php
@@ -4,43 +4,44 @@ $errors = array();
 $message = '';
 if(isset($_POST['submit'])){
 
-  $username = isset($_POST['medium-input'])?$_POST['medium-input']:'';
-  $Price = isset($_POST['small-input'])?$_POST['small-input']:'';
+$username = isset($_POST['name'])?$_POST['name']:'';
+$price = isset($_POST['price'])?$_POST['price']:'';
 //   $image = isset($_POST['small-medium'])?$_POST['small-medium']:'';
-  $category = isset($_POST['dropdown'])?$_POST['dropdown']:'';
-  $Tags = isset($_POST['Tags'])?$_POST['Tags']:'';
-  $Description = isset($_POST['textfield'])?$_POST['textfield']:'';
-  $image = $_FILES['small-medium']['name'];
-  $source = $_FILES['small-medium']['tmp_name'];
+$category = isset($_POST['dropdown'])?$_POST['dropdown']:'';
+$Tags = isset($_POST['Tags'])?$_POST['Tags']:'';
+$description = isset($_POST['description'])?$_POST['description']:'';
+$image = $_FILES['image']['name'];
+$source = $_FILES['image']['tmp_name'];
 // echo $Price;
 // echo $username;
+// echo "uname:".$username.", " . $price .", " . $category .", " . $description .", " .$image; 
 // die();
-    if (move_uploaded_file($source, "productImage/".$image)) {
-        
-        $sql = "INSERT INTO product(`name`,`price`,`image`,`long_description`,`category_id`)VALUES('$username', '$Price' , '$image', '$Description',  '$category')" ;
-    } else {
-        echo "Image NOt Upload";
-    }
+if (move_uploaded_file($source, "productImage/".$image)) {
+
+$sql = "INSERT INTO product(`name`,`price`,`image`,`long_description`,`category_id`)VALUES('$username', '$price' , '$image', '$description',  '$category')" ;
+//echo $sql;
+} else {
+echo "Image NOt Upload";
+}
 // echo $sql;
-  if ($conn->query($sql) === true) {
-      header('location:index.php');
-   "New record created successfully";
-  } else {
-    $errors[] = array('input'=>'form','msg'=>$conn->error);
-   "Error: " . $sql . "<br>" . $conn->error;
-  }
-  
-  
-  $conn->close();
+if ($conn->query($sql) === true) {
+header('location:index.php');
+"New record created successfully";
+} else {
+$errors[] = array('input'=>'form','msg'=>$conn->error);
+"Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
 
 
 }
 ?>
-   <?php if (sizeof($errors)>0): ?>
-   <ul>
-   <?php foreach ($errors as $error) : ?>
-   <li><?php echo $error['msg']; ?></li>
-   <?php endforeach; ?>
-   </ul>
-   <?php endif; ?>
- 
+<?php if (sizeof($errors)>0): ?>
+<ul>
+<?php foreach ($errors as $error) : ?>
+<li><?php echo $error['msg']; ?></li>
+<?php        endforeach;  ?>
+</ul>
+<?php endif; ?>
