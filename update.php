0e326e334ccfb6714ab39bef38a88fdd3a08f0b1
abhisheksaurabh7785 <72158831+abhisheksaurabh7785@users.noreply.github.com>
2020-10-20 11:56:53 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,47 +1,28 @@
 <?php
 include ('config.php');
-$errors = array();
-$message = '';
-if(isset($_POST['submit'])){
 
-$username = isset($_POST['name'])?$_POST['name']:'';
-$price = isset($_POST['price'])?$_POST['price']:'';
-//   $image = isset($_POST['small-medium'])?$_POST['small-medium']:'';
-$category = isset($_POST['dropdown'])?$_POST['dropdown']:'';
-$Tags = isset($_POST['Tags'])?$_POST['Tags']:'';
-$description = isset($_POST['description'])?$_POST['description']:'';
+// echo "hii";
+
+$name = $_POST['name'];
+$price = $_POST['price'];
+$dropdown = $_POST['dropdown'];
+$tags = implode(",", $_POST['fashion']);
+$description = $_POST['description'];
+$color = implode(",", $_POST['color']);
+
 $image = $_FILES['image']['name'];
-$source = $_FILES['image']['tmp_name'];
-// echo $Price;
-// echo $username;
-// echo "uname:".$username.", " . $price .", " . $category .", " . $description .", " .$image; 
-// die();
-if (move_uploaded_file($source, "productImage/".$image)) {
+$desc = $_FILES['image']['tmp_name'];
 
-$sql = "INSERT INTO product(`name`,`price`,`image`,`long_description`,`category_id`)VALUES('$username', '$price' , '$image', '$description',  '$category')" ;
-//echo $sql;
-} else {
-echo "Image NOt Upload";
-}
+if (move_uploaded_file($desc, "productImage/".$image)) {
+$sql = "INSERT INTO product (`name`, `price`, `image`, `long_description`, `tags`, `category`, `color`) VALUES ('$name', '$price', '$image', '$description', '$tags', '$dropdown', '$color')";
 // echo $sql;
-if ($conn->query($sql) === true) {
-header('location:index.php');
-"New record created successfully";
+// exit();
+mysqli_query($conn, $sql);
+header('LOCATION:products.php');
 } else {
-$errors[] = array('input'=>'form','msg'=>$conn->error);
-"Error: " . $sql . "<br>" . $conn->error;
+echo "file not uploaded";
 }
 
 
-$conn->close();
 
-
-}
 ?>
-<?php if (sizeof($errors)>0): ?>
-<ul>
-<?php foreach ($errors as $error) : ?>
-<li><?php echo $error['msg']; ?></li>
-<?php        endforeach;  ?>
-</ul>
-<?php endif; ?>
